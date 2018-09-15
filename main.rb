1d26e67399c9affd38f893e9ef7234a75888ee46
Matthew Haney <thewpaney@gmail.com>
2018-09-15 12:28:09 -0700

    Added preliminary PWM

--- main.rb
+++ main.rb
@@ -13,6 +13,10 @@ class BBBIOT < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
 
+  before do
+    puts params.inspect
+  end
+
   valid_actions = ["index", "gpio"]
 
   get '/' do
@@ -25,17 +29,18 @@ class BBBIOT < Sinatra::Base
 
   get '/gpio' do
     @pins = (11..31).collect {|e| "P9_#{e}"} + (41..42).collect {|e| "P9_#{e}"} + (3..46).collect {|e| "P8_#{e}"}
-    # @pins = ["P9_12"] # testing
     erb :gpio
   end
 
+  get '/pwm' do
+    @pins = [14,16,21,22].collect {|e| "P9_#{e}"} + [13,19].collect {|e| "P8_#{e}"}
+    erb :pwm
+  end
+
   post '/gpio' do
-    puts params.inspect
     # Needs to have format: P[8|9]_[1-46]
-#    valid = /P[8|9]_([0-9][0-9]?)\z/.match(params[:pin])
- #   return "bad pin" if valid.nil? or valid[1].to_i > 46 or valid[1].to_i < 0
-  #  puts valid.inspect
-    # pin = valid[0].to_sym
+    valid = /P[8|9]_([0-9][0-9]?)\z/.match(params[:pin])
+    return "pin not GPIO enabled" if valid.nil? or valid[1].to_i > 46 or valid[1].to_i < 0
     pin = params[:pin].to_sym
     case params[:action]
     when "read"
@@ -50,6 +55,40 @@ class BBBIOT < Sinatra::Base
     when "disable"
       Beaglebone::disable_pin(pin)
       return "Disabled"
+    else
+      return "Bad action"
+    end
+  end
+
+  post '/pwm' do
+    valid = /P([8|9])_([0-9][0-9]?)\z/.match(params[:pin])
+    if valid.nil? or (valid[1] == "8" and not ([13,19].include? valid[2])) or (valid[1] == "9" and not ([14,16,21,22].include? valid[2]))
+      return "pin not PWM enabled"
+    end
+    pin = params[:pin].to_sym
+    case params[:action]
+    when "disable"
+      BeagleBone::disable_pin(pin)
+      return "Disabled"
+    when "start"
+      PWM.start(pin, params[:f].to_i, params[:dc].to_i, params[:p] ? params[:p].to_sym : :NORMAL)
+    when "set"
+      params.each do |k,v|
+        case k
+        when "f"
+          PWM.set_frequency(pin, v.to_i)
+        when "dc"
+          PWM.set_duty_cycle(pin, v.to_i)
+        when "t_ns"
+          PWM.set_period_ns(pin, v.to_i)
+        when "dc_ns"
+          PWM.set_duty_cycle_ns(pin, v.to_i)
+        when "p"
+          PWM.set_polarity(pin, v.to_sym)
+        end
+      end
+    else
+      return "Bad action"
     end
   end
   
